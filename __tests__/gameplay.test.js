dff1087130d43f615181a077171f0704760d21c3
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-02-03 10:49:20 -0800

    Pass test for levelOneRoute method
    Co-authored-by: Daniel Lindsey <dlinds@users.noreply.github.com>

--- __tests__/gameplay.test.js
+++ __tests__/gameplay.test.js
@@ -12,4 +12,9 @@ describe("GamePlay", () => {
     expect(myGame.secondLevelRoute).toEqual(0);
     expect(myGame.thirdLevelRoute).toEqual(0);
   });
+  it("should update first level route and return true or false based on user input", () => {
+    expect(myGame.levelOneRoute("Fire")).toEqual(true);
+    expect(myGame.levelOneRoute("Water")).toEqual(true);
+    expect(myGame.levelOneRoute("Neither")).toEqual(false);
+  });
 });
--- src/js/gameplay.js
+++ src/js/gameplay.js
@@ -5,4 +5,30 @@ export default class GamePlay {
     this.secondLevelRoute = 0;
     this.thirdLevelRoute = 0;
   }
+  levelOneRoute(myChoice) {
+    switch (myChoice) {
+      case "Fire":
+        this.firstLevelRoute = "Fire";
+        return true;
+      case "Water":
+        this.firstLevelRoute = "Water";
+        return true;
+      default:
+        this.firstLevelRoute = "Neither";
+        return false;
+    }
+  }
 }
+
+/* you make a choice, fire or water
+currentLevel is set to 1
+firstLevelRoute is set to fire/water
+
+UI logic checks to see what current level is
+if 1, checks to see if fire or water
+presents options 
+
+
+
+
+*/
